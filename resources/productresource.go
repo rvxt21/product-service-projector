93fbfbe85b9671174aed2adec3a37baeadedcfb1
Oksana Hladunska <oksana.hladunska@gmail.com>
2024-08-05 14:01:49 +0300

    add AdminMiddleware

--- resources/productresource.go
+++ resources/productresource.go
@@ -19,21 +19,22 @@ type ProductsResourse struct {
 }
 
 func (tr *ProductsResourse) RegisterRoutes(r *mux.Router) {
-	r.Handle("/products/{id}", middleware.IdMiddleware(http.HandlerFunc(tr.DeleteProduct))).Methods("DELETE")
-	r.HandleFunc("/products/{id}", tr.UpdateProduct).Methods("PUT")
-	r.Handle("/products/availability/{id}", middleware.IdMiddleware(http.HandlerFunc(tr.UpdateAvailability))).Methods("PATCH")
-	r.HandleFunc("/products", tr.CreateProduct).Methods("POST")
-	r.HandleFunc("/products/{id}", tr.GetProductByID).Methods("GET")
+	r.Use(middleware.MockAuthenticationMiddleware)
+	
+	r.Handle("/products/{id}", middleware.AdminMiddleware(middleware.IdMiddleware(http.HandlerFunc(tr.DeleteProduct)))).Methods("DELETE")
+        r.Handle("/product/{id}", middleware.AdminMiddleware(http.HandlerFunc(tr.UpdateProduct))).Methods("PUT")
+	r.Handle("/products/availability/{id}", middleware.AdminMiddleware(middleware.IdMiddleware(http.HandlerFunc(tr.UpdateAvailability)))).Methods("PATCH")
+        r.Handle("/products", middleware.AdminMiddleware(http.HandlerFunc(tr.CreateProduct))).Methods("POST")
+
 	r.HandleFunc("/products", tr.GetAllProducts).Methods("GET")
 	r.HandleFunc("/products/by-ids", tr.GetProductsByIDS).Methods("GET")
-	//r.Handle("/products/{id}", middleware.IdMiddleware(http.HandlerFunc(tr.GetByID))).Methods("GET")
 
-	r.HandleFunc("/categories", tr.CreateCategory).Methods("POST")
+	r.Handle("/categories", middleware.AdminMiddleware(http.HandlerFunc(tr.CreateCategory))).Methods("POST")
 	r.HandleFunc("/categories", tr.GetAllCategories).Methods("GET")
 	r.Handle("/categories/{idCategory}", middleware.IdMiddlewareCategory(http.HandlerFunc(tr.GetCategoryByID))).Methods("GET")
-	r.Handle("/categories/{idCategory}", middleware.IdMiddlewareCategory(http.HandlerFunc(tr.UpdateCategory))).Methods("PUT")
-	r.Handle("/categories/{idCategory}", middleware.IdMiddlewareCategory(http.HandlerFunc(tr.DeleteCategory))).Methods("DELETE")
-
+	r.Handle("/categories/{idCategory}", middleware.AdminMiddleware(middleware.IdMiddlewareCategory(http.HandlerFunc(tr.UpdateCategory)))).Methods("PUT")
+	r.Handle("/categories/{idCategory}", middleware.AdminMiddleware(middleware.IdMiddlewareCategory(http.HandlerFunc(tr.DeleteCategory)))).Methods("DELETE")
+	r.HandleFunc("/product/{id}", tr.GetProductByID).Methods("GET")
 } //alternative for register routes
 
 func (tr *ProductsResourse) CreateProduct(w http.ResponseWriter, r *http.Request) {
