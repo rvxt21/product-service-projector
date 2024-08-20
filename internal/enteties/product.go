695d3f7b830efc74136ccac881b4e47806fa3e5c
rvxt21 <anastasia1koryagina@gmail.com>
2024-08-20 14:59:14 +0300

    add validation for price

--- internal/enteties/product.go
+++ internal/enteties/product.go
@@ -1,5 +1,7 @@
 package enteties
 
+import "errors"
+
 type Product struct {
 	ID          int    `json:"id"`
 	Name        string `json:"name"`
@@ -8,4 +10,20 @@ type Product struct {
 	Quantity    int    `json:"quantity"`
 	Category    int    `json:"category"`
 	IsAvailable bool   `json:"is_available"`
+}
+
+var (
+	ErrNegativePriceValue = errors.New("negative price")
+	ErrZeroPriceValue     = errors.New("zero price value")
+)
+
+func (p Product) IsValidPrice() error {
+	switch {
+	case p.Price < 0:
+		return ErrNegativePriceValue
+	case p.Price == 0:
+		return ErrZeroPriceValue
+	default:
+		return nil
+	}
 }
